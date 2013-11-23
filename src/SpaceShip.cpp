4b0e709e4a0ea8d577a893eeb141ffc35e346977
sheva29 <design@sanchezmauricio.com>
2013-11-22 20:43:37 -0500

    Added the spaceship class

---
+++ src/SpaceShip.cpp
@@ -0,0 +1,9 @@
+//
+//  SpaceShip.cpp
+//  Space_Odyssey_2
+//
+//  Created by Mauricio Sanchez Duque on 11/22/13.
+//
+//
+
+#include "SpaceShip.h"
---
+++ src/SpaceShip.h
@@ -0,0 +1,25 @@
+//
+//  SpaceShip.h
+//  Space_Odyssey_2
+//
+//  Created by Mauricio Sanchez Duque on 11/22/13.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+
+class SpaceShip {
+    
+    public:
+    
+    SpaceShip();
+    void draw();
+    void update();
+    
+    ofVec2f pos, vel;
+    
+    
+        
+        
+};
